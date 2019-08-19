3f024d675c36a85c6316f8cb82583781d836f88a
farajsharman <farajsharman@yahoo.com>
2019-08-19 12:18:47 +0100

    refactor code for pull request

--- src/Tv_shows.js
+++ src/Tv_shows.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 function Tv_shows(props) {
-    return (
-      <Link to={`/${props.id}`} className='item'>
-        <img src={props.cover} alt={props.name} />
-        <div className='overlay'>
-          <h1>{props.name}</h1>
-        </div>
-      </Link>     
+  return (
+    <Link to={`/${props.id}`} className="item">
+      <img src={props.cover} alt={props.name} />
+      <div className="overlay">
+        <h1>{props.name}</h1>
+      </div>
+    </Link>
   );
 }
 
