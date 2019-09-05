891f1a8f6ae69f40e770870cdf2beaabfcd7457a
farajsharman <farajsharman@yahoo.com>
2019-09-05 11:59:09 +0100

    task 25 completed

--- src/Frontend/Details/Details.js
+++ src/Frontend/Details/Details.js
@@ -1,49 +1,53 @@
 import React, { useState, useEffect } from "react";
 import { Link, Redirect } from "react-router-dom";
-// import getGallery from "../gallery-get.js";
+import Loading from "../common/Loading.js";
+//import getGallery from "../gallery-get.js";
 // import "./Details.css";
 export default function Details({ match }) {
-  const [show, setShow] = useState({}); 
+  const [show, setShow] = useState({});
 
   useEffect(() => {
-    fetch('/rest/shows')
-            .then(response => response.json())
-            .then(shows => {
-                let showId = match.params.showId;
-                let show = shows.find(show => show.id === showId);
-                setShow( show );
-    });
+    fetch("/rest/shows")
+      .then(response => response.json())
+      .then(shows => {
+        let showId = match.params.showId;
+        let show = shows.find(show => show.id === showId);
+        setShow(show);
+      });
   }, [match.params.showId]);
-    
-    // let tvshowtitle = getGallery().find(function (tvshow) {
-    //   return tvshow.id === tvshowid;
-    // }),name;
-  //   tvShow
-  //     ? this.setState({
-  //         title: tvShow.name,
-  //         details: tvShow.details,
-  //         cover: tvShow.cover
-  //       })
-  //     : this.setState({ title: undefined });
 
-    if (show === undefined) {
-      return <Redirect to="/not-found" />;
-    } 
-    else if (show.id) {
-      return (
-        <div className="Details">
-          <h1>{show.title}</h1>
-          <div className="container">
-            <div className="text">{show.synopsis}</div>
-            <img
-              className="image"
-              src={require(`../common/images/${show.id}.jpg`)}
-              alt={show.title}
-            />
-          </div>
-          <Link to="/">Back to home page</Link>
-        </div>
-      );
-    }
-    return <div></div>;
-  }
+  // let tvshowtitle = getGallery().find(function (tvshow) {
+  //       return tvshow.id === tvshowid;
+  //     }),name;
+  //     tvShow
+  //       ? this.setState({
+  //           title: tvShow.name,
+  //           details: tvShow.details,
+  //           cover: tvShow.cover
+  //         })
+  //       : this.setState({ title: undefined });
+  //   return (
+
+  //   );
+  // }
+  return show === undefined ? (
+    <Redirect to="/not-found" />
+  ) : show.id ? (
+    <div className="Details">
+      <h1>{show.title}</h1>
+      <div className="container">
+        <div className="text">{show.synopsis}</div>
+        <img
+          className="image"
+          src={require(`../common/images/${show.id}.jpg`)}
+          alt={show.title}
+        />
+      </div>
+      <Link to="/">Back to home page</Link>
+    </div>
+  ) : (
+    <div>
+      <Loading />
+    </div>
+  );
+}
--- src/Frontend/Gallery.js
+++ src/Frontend/Gallery.js
@@ -1,38 +1,38 @@
-import React, {useEffect,useState} from 'react';
-import TvShows from './Tv_shows';
+import React, { useEffect, useState } from "react";
+import TvShows from "./Tv_shows";
+import Loading from "./common/Loading.js";
+
 // import getGallery from './gallery-get'
 // export default class Gallery extends React.Component {
 
-  
 function Gallery() {
   const [gallery, setGallery] = useState([]);
   useEffect(() => {
-    fetch('/rest/shows')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (myJson) {
-            setGallery(myJson)
-        });
+    fetch("/rest/shows")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        setGallery(myJson);
+      });
   });
 
-  return (
+  return gallery && gallery.length ? (
     <div>
-      <div className='container'>
-        {
-          gallery.map(tvshows => (
-            
-            <TvShows
-              key={tvshows.id}
-              id={tvshows.id}
-              name={tvshows.name} 
-              cover={tvshows.cover} />
-          ))
-        }      
+      <div className="container">
+        {gallery.map(tvshows => (
+          <TvShows
+            key={tvshows.id}
+            id={tvshows.id}
+            name={tvshows.name}
+            cover={tvshows.cover}
+          />
+        ))}
       </div>
-    </div>     
+    </div>
+  ) : (
+    <Loading />
   );
 }
 
-  
-  export default Gallery;
+export default Gallery;
